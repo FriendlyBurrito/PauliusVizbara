9025eb63bd2d3c090595a42729f4c1dabc96edd5
PauliusVizbara <paulius1207@gmail.com>
2019-04-04 22:51:59 +0300

    Finish first version

--- main.js
+++ main.js
@@ -1,4 +1,52 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-
-
+$(function() {
+   $("#lastcard").css('margin-bottom', innerHeight);
 });
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slide");
+    var dots = document.getElementsByClassName("sliderDot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+
+/* Scrolling */
+
+
+window.addEventListener("scroll", function(){
+    if ( innerWidth < 1200) return;
+    var doc = document.documentElement;
+    var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+
+    if ( top > 10){
+        $("#insideMenu").removeClass("extended");
+
+    }
+    else{
+
+        $("#insideMenu").addClass("extended");
+    }
+}, false)
